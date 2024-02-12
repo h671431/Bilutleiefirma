010a96883939a2df4d0fd0c55270962e5cc641eb
Mikal Reed-Larsen <585124@stud.hvl.com>
2024-02-12 14:05:40 +0100

    tester for firma

--- test/src/MainTest.java
+++ test/src/MainTest.java
@@ -4,13 +4,18 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.List;
+
 public class MainTest {
 
     private Adresse adresse;
+    private Firma firma;
+
 
     @Before
     public void setUp() throws Exception {
         adresse = new Adresse("Testveien 1", "1234", "Testby");
+        firma = new Firma("TestFirma", "12345678", adresse);
     }
 
     @Test
@@ -43,4 +48,44 @@ public class MainTest {
         String expected = "Testveien 1, 1234 Testby";
         assertEquals(expected, adresse.toString());
     }
+    @Test
+    public void testFirmaInstantiering() {
+        assertEquals("TestFirma", firma.getNavn());
+        assertEquals("12345678", firma.getTelefon());
+        assertEquals(adresse, firma.getAdresse());
+    }
+
+    @Test
+    public void testLeggTilUtleiekontor() {
+        Kontor nyttKontor = new Kontor("TestKontor", "87654321", new Adresse("Kontorveien 2", "8765", "Kontorby"));
+        firma.leggTilUtleiekontor(nyttKontor);
+        List<Kontor> kontorListe = firma.getKontor();
+
+        assertTrue(kontorListe.contains(nyttKontor));
+    }
+    @Test
+    public void testSetNavn() {
+        firma.setNavn("NyttFirmaNavn");
+        assertEquals("NyttFirmaNavn", firma.getNavn());
+    }
+    @Test
+    public void testSetTelefon() {
+        firma.setTelefon("99887766");
+        assertEquals("99887766", firma.getTelefon());
+    }
+
+    @Test
+    public void testSetAdresse() {
+        Adresse nyAdresse = new Adresse("NyFirmavei 3", "4321", "NyFirmaby");
+        firma.setAdresse(nyAdresse);
+        assertEquals(nyAdresse, firma.getAdresse());
+    }
+
+
+
+
+
+
+
+
 }
